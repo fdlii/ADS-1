faf7583e7047411e6c90844521007d30cac4d7f4
fdlii <155849941+fdlii@users.noreply.github.com>
2024-02-25 14:56:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,57 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value == 1) return false;
+  for (int i = 2; i < value; i++) {
+	  if (value % i == 0) {
+		  return false;
+	  }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	int cur = 0;
+	if (n == 1) return 2;
+	for (int i = 2;; i++) {
+		bool f = 1;
+		for (int j = 2; j < i; j++) {
+			if (i % j == 0) {
+				f = 0;
+				break;
+			}
+		}
+		if (f == 1) cur += 1;
+		if (cur == n) return i;
+	}
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	int i = value + 1;
+	for (i;; i++) {
+		bool f = 1;
+		for (int j = 2; j < i; j++) {
+			if (i % j == 0) {
+				f = 0;
+				break;
+			}
+		}
+		if (f == 1) break;
+	}
+	return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	int sum = 0;
+	for (int i = 2; i < hbound; i++) {
+		bool f = 1;
+		for (int j = 2; j < i; j++) {
+			if (i % j == 0) {
+				f = 0;
+				break;
+			}
+		}
+		if (f == 1) sum += i;
+	}
+	return sum;
 }
